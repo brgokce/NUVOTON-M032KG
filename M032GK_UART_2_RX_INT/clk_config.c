07bf66aee6ed6e3987891b67a8ff061c4c2f042c
Dr. Barış GÖKÇE <38115377+brgokce@users.noreply.github.com>
2025-02-25 13:49:20 +0300

    Add files via upload

---
+++ M032GK_UART_2_RX_INT/clk_config.c
@@ -0,0 +1,143 @@
+/****************************************************************************
+ * @file     clk_config.c
+ * @version  V1.09.0002
+ * @Date     Thu Feb 13 2025 23:26:37 GMT+0300 (GMT+03:00)
+ * @brief    NuMicro generated code file
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (C) 2013-2025 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+
+/********************
+MCU:M032KG8AE(LQFP128)
+Base Clocks:
+HIRC:48MHz
+HXT:32MHz
+PLL:72MHz
+HCLK:72MHz
+PCLK0:72MHz
+PCLK1:36MHz
+Enabled-Module Frequencies:
+ISP=Bus Clock(HCLK):72MHz/Engine Clock:48MHz
+TMR0=Bus Clock(PCLK0):72MHz/Engine Clock:72MHz
+UART0=Bus Clock(PCLK0):72MHz/Engine Clock:72MHz
+********************/
+
+#include "M031Series.h"
+#include "clk_config.h"
+
+void clk_config_init_isp(void)
+{
+    CLK_EnableModuleClock(ISP_MODULE);
+
+    return;
+}
+
+void clk_config_deinit_isp(void)
+{
+    CLK_DisableModuleClock(ISP_MODULE);
+
+    return;
+}
+
+void clk_config_init_tmr0(void)
+{
+    CLK_EnableModuleClock(TMR0_MODULE);
+    CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_PCLK0, MODULE_NoMsk);
+
+    return;
+}
+
+void clk_config_deinit_tmr0(void)
+{
+    CLK_DisableModuleClock(TMR0_MODULE);
+
+    return;
+}
+
+void clk_config_init_uart0(void)
+{
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_PCLK0, CLK_CLKDIV0_UART0(1));
+
+    return;
+}
+
+void clk_config_deinit_uart0(void)
+{
+    CLK_DisableModuleClock(UART0_MODULE);
+
+    return;
+}
+
+void clk_config_init_base(void)
+{
+    /* If the macros do not exist in your project, please refer to the related clk.h in Header folder of the tool package */
+    /* Enable clock source */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk|CLK_PWRCTL_HXTEN_Msk);
+
+    /* Waiting for clock source ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk|CLK_STATUS_HXTSTB_Msk);
+
+    /* Disable PLL first to avoid unstable when setting PLL */
+    CLK_DisablePLL();
+
+    /* Set PLL frequency */
+    CLK->PLLCTL = (CLK->PLLCTL & ~(0x000FFFFFUL)) | 0x0000C219UL;
+
+    /* Waiting for PLL ready */
+    CLK_WaitClockReady(CLK_STATUS_PLLSTB_Msk);
+
+    /* Set HCLK clock */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_PLL, CLK_CLKDIV0_HCLK(1));
+
+    /* Set PCLK-related clock */
+    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV1 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    return;
+}
+
+void clk_config_init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+    //CLK->PWRCTL   = (CLK->PWRCTL   & ~(0x0000000FUL)) | 0x02310015UL;
+    //CLK->PLLCTL   = (CLK->PLLCTL   & ~(0x000FFFFFUL)) | 0x0000C219UL;
+    //CLK->CLKDIV0  = (CLK->CLKDIV0  & ~(0x00FFFFFFUL)) | 0x00000000UL;
+    //CLK->CLKDIV4  = (CLK->CLKDIV4  & ~(0x00FFFFFFUL)) | 0x00000000UL;
+    //CLK->PCLKDIV  = (CLK->PCLKDIV  & ~(0x00000077UL)) | 0x00000010UL;
+    //CLK->CLKSEL0  = (CLK->CLKSEL0  & ~(0x0000013FUL)) | 0x0000003AUL;
+    //CLK->CLKSEL1  = (CLK->CLKSEL1  & ~(0x7777777FUL)) | 0x4472723BUL;
+    //CLK->CLKSEL2  = (CLK->CLKSEL2  & ~(0x0030033FUL)) | 0x0020032BUL;
+    //CLK->CLKSEL3  = (CLK->CLKSEL3  & ~(0x77777700UL)) | 0x44444400UL;
+    //CLK->AHBCLK   = (CLK->AHBCLK   & ~(0x0000009EUL)) | 0x00000004UL;
+    //CLK->APBCLK0  = (CLK->APBCLK0  & ~(0x18FF33FFUL)) | 0x00010004UL;
+    //CLK->APBCLK1  = (CLK->APBCLK1  & ~(0x000F0300UL)) | 0x00000000UL;
+    //CLK->CLKOCTL  = (CLK->CLKOCTL  & ~(0x0000007FUL)) | 0x00000000UL;
+    //SysTick->CTRL = (SysTick->CTRL & ~(0x00000005UL)) | 0x00000000UL;
+    //RTC->LXTCTL   = (RTC->LXTCTL   & ~(0x00000080UL)) | 0x00000000UL;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable base clock */
+    clk_config_init_base();
+
+    /* Enable module clock and set clock source */
+    clk_config_init_isp();
+    clk_config_init_tmr0();
+    clk_config_init_uart0();
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+
+    return;
+}
+
+/*** (C) COPYRIGHT 2013-2025 Nuvoton Technology Corp. ***/
