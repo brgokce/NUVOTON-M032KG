99e730eb96f43bd7bba200410556f198b7116b00
Dr. Barış GÖKÇE <38115377+brgokce@users.noreply.github.com>
2025-03-03 15:45:53 +0300

    Add files via upload

---
+++ M032GK_ADC_1/pin_config.h
@@ -0,0 +1,38 @@
+/****************************************************************************
+ * @file     pin_config.h
+ * @version  V1.33.0005
+ * @Date     Mon Mar 03 2025 15:04:28 GMT+0300 (GMT+03:00)
+ * @brief    NuMicro generated code file
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Copyright (C) 2013-2025 Nuvoton Technology Corp. All rights reserved.
+*****************************************************************************/
+
+#ifndef __PIN_CONFIG_H__
+#define __PIN_CONFIG_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+void pin_config_init_adc0(void);
+void pin_config_deinit_adc0(void);
+void pin_config_init_ice(void);
+void pin_config_deinit_ice(void);
+void pin_config_init_pb(void);
+void pin_config_deinit_pb(void);
+void pin_config_init_pc(void);
+void pin_config_deinit_pc(void);
+void pin_config_init_pf(void);
+void pin_config_deinit_pf(void);
+void pin_config_init_uart0(void);
+void pin_config_deinit_uart0(void);
+void pin_config_init(void);
+void pin_config_deinit(void);
+#ifdef __cplusplus
+}
+#endif
+#endif /*__PIN_CONFIG_H__*/
+
+/*** (C) COPYRIGHT 2013-2025 Nuvoton Technology Corp. ***/
